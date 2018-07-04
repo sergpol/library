bdffdf7c02723793877c413db943f09fbd3b7a9b
p.sergey <sergpol20@gmail.com>
2018-07-04 15:48:20 +0300

    +

--- library/AlertModule.swift
+++ library/AlertModule.swift
@@ -9,10 +9,8 @@
 import Foundation
 import UIKit
 
-public class AlertModule: NSObject {
-    let shared = AlertModule()
-    
-    public func showAlert(onViewController: UIViewController) {
+open class AlertModule: NSObject {
+    open func showAlert(onViewController: UIViewController) {
         let alertController = UIAlertController(title: "Title", message: "Message", preferredStyle: UIAlertControllerStyle.alert)
         alertController.addAction(UIAlertAction(title: "Yes", style: UIAlertActionStyle.default) {(_) in
         
