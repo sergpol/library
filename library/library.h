f8042057514a143a77cbee2da39cafde9c6ea41e
p.sergey <sergpol20@gmail.com>
2018-07-04 14:00:31 +0300

    Initial Commit

---
+++ library/library.h
@@ -0,0 +1,19 @@
+//
+//  library.h
+//  library
+//
+//  Created by Сергей Полицинский on 04.07.2018.
+//  Copyright © 2018 PTK. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for library.
+FOUNDATION_EXPORT double libraryVersionNumber;
+
+//! Project version string for library.
+FOUNDATION_EXPORT const unsigned char libraryVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <library/PublicHeader.h>
+
+
